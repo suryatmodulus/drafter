f515afcb9b28bda3bcd60465a01b453bcdbf75b8
Luiz Aoqui <luizaoqui@loopholelabs.io>
2024-08-12 21:30:47 -0400

    nat: move logic in closures to struct

--- pkg/roles/nat.go
+++ pkg/roles/nat.go
@@ -27,19 +27,6 @@ var (
 	ErrNATContextCancelled                  = errors.New("context for NAT cancelled")
 )
 
-type claimableNamespace struct {
-	namespace *network.Namespace
-	claimed   bool
-}
-
-type Namespaces struct {
-	ReleaseNamespace func(namespace string) error
-	ClaimNamespace   func() (string, error)
-
-	Wait  func() error
-	Close func() error
-}
-
 type CreateNamespacesHooks struct {
 	OnBeforeCreateNamespace func(id string)
 	OnBeforeRemoveNamespace func(id string)
@@ -66,8 +53,7 @@ func CreateNAT(
 	allowIncomingTraffic bool,
 
 	hooks CreateNamespacesHooks,
-) (namespaces *Namespaces, errs error) {
-	namespaces = &Namespaces{}
+) (namespaces *NamespacesManager, errs error) {
 
 	// We use the background context here instead of the internal context because we want to distinguish
 	// between a context cancellation from the outside and getting a response
@@ -111,82 +97,22 @@ func CreateNAT(
 		panic(ErrNotEnoughAvailableIPsInNamespaceCIDR)
 	}
 
-	var (
-		hostVeths     []*network.IPPair
-		hostVethsLock sync.Mutex
-
-		namespaceVeths     []*network.IP
-		namespaceVethsLock sync.Mutex
-
-		claimableNamespaces     = map[string]claimableNamespace{}
-		claimableNamespacesLock sync.Mutex
-	)
-
-	var closeLock sync.Mutex
-	closed := false
-
-	closeInProgressContext, cancelCloseInProgressContext := context.WithCancel(closeCtx) // We use `closeContext` here since this simply intercepts `ctx`
-	namespaces.Close = func() (errs error) {
-		defer cancelCloseInProgressContext()
-
-		hostVethsLock.Lock()
-		defer hostVethsLock.Unlock()
-
-		for _, hostVeth := range hostVeths {
-			if err := namespaceVethIPs.ReleasePair(closeCtx, hostVeth); err != nil {
-				errs = errors.Join(errs, ErrCouldNotReleaseHostVethIP, err)
-			}
-		}
-
-		hostVeths = []*network.IPPair{}
-
-		namespaceVethsLock.Lock()
-		defer namespaceVethsLock.Unlock()
-
-		for _, namespaceVeth := range namespaceVeths {
-			if err := namespaceVethIPs.ReleaseIP(closeCtx, namespaceVeth); err != nil {
-				errs = errors.Join(errs, ErrCouldNotReleaseNamespaceVethIP, err)
-			}
-		}
-
-		namespaceVeths = []*network.IP{}
-
-		claimableNamespacesLock.Lock()
-		defer claimableNamespacesLock.Unlock()
-
-		for _, claimableNamespace := range claimableNamespaces {
-			if hook := hooks.OnBeforeRemoveNamespace; hook != nil {
-				hook(claimableNamespace.namespace.GetID())
-			}
-
-			if err := claimableNamespace.namespace.Close(); err != nil {
-				errs = errors.Join(errs, ErrCouldNotCloseNamespace, err)
-			}
-		}
-
-		claimableNamespaces = map[string]claimableNamespace{}
-
-		closeLock.Lock()
-		defer closeLock.Unlock()
-
-		if !closed {
-			closed = true
-
-			if err := network.RemoveNAT(hostInterface); err != nil {
-				errs = errors.Join(errs, ErrCouldNotRemoveNAT, err)
-			}
-		}
-
-		// No need to call `.Wait()` here since `.Wait()` is just waiting for us to cancel the in-progress context
-
-		return
-	}
-	// Future-proofing; if we decide that NATing should use a background copy loop like `socat`, we can wait for that loop to finish here and return any errors
-	namespaces.Wait = func() error {
-		<-closeInProgressContext.Done()
-
-		return nil
-	}
+	namespaces = newNamespacesManager(
+		namespacesManagerConfig{
+			ctx:                       internalCtx,
+			closeCtx:                  closeCtx,
+			hostInterface:             hostInterface,
+			namespaceInterface:        namespaceInterface,
+			namespaceInterfaceGateway: namespaceInterfaceGateway,
+			namespaceInterfaceNetmask: namespaceInterfaceNetmask,
+			namespaceInterfaceIP:      namespaceInterfaceIP,
+			namespaceInterfaceMAC:     namespaceInterfaceMAC,
+			blockedSubnetCIDR:         blockedSubnetCIDR,
+			allowIncomingTraffic:      allowIncomingTraffic,
+			hostVethIPs:               hostVethIPs,
+			namespaceVethIPs:          namespaceVethIPs,
+			hooks:                     hooks,
+		})
 
 	// We intentionally don't call `wg.Add` and `wg.Done` here - we are ok with leaking this
 	// goroutine since we return the Close func. We still need to `defer handleGoroutinePanic()()` however so that
@@ -212,154 +138,252 @@ func CreateNAT(
 	})
 
 	for i := uint64(0); i < availableIPs; i++ {
+		select {
+		case <-ctx.Done():
+			panic(ctx.Err())
+		default:
+		}
+
 		id := fmt.Sprintf("%v%v", namespacePrefix, i)
-
-		var hostVeth *network.IPPair
-		if err := func() error {
-			hostVethsLock.Lock()
-			defer hostVethsLock.Unlock()
-
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-
-			default:
-				break
-			}
-
-			var err error
-			hostVeth, err = hostVethIPs.GetPair(internalCtx)
-			if err != nil {
-				if e := namespaceVethIPs.ReleasePair(closeCtx, hostVeth); e != nil {
-					return errors.Join(ErrCouldNotReleaseHostVethIP, err, e)
-				}
-
-				return err
-			}
-
-			hostVeths = append(hostVeths, hostVeth)
-
-			return nil
-		}(); err != nil {
-			panic(errors.Join(ErrCouldNotOpenHostVethIPs, err))
+		if err := namespaces.addClaimableNamespace(id); err != nil {
+			panic(err)
 		}
-
-		var namespaceVeth *network.IP
-		if err := func() error {
-			namespaceVethsLock.Lock()
-			defer namespaceVethsLock.Unlock()
-
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-
-			default:
-				break
-			}
-
-			var err error
-			namespaceVeth, err = namespaceVethIPs.GetIP(internalCtx)
-			if err != nil {
-				if e := namespaceVethIPs.ReleaseIP(closeCtx, namespaceVeth); e != nil {
-					return errors.Join(ErrCouldNotReleaseNamespaceVethIP, err, e)
-				}
-
-				return err
-			}
-
-			namespaceVeths = append(namespaceVeths, namespaceVeth)
-
-			return nil
-		}(); err != nil {
-			panic(errors.Join(ErrCouldNotOpenNamespaceVethIPs, err))
-		}
-
-		if err := func() error {
-			claimableNamespacesLock.Lock()
-			defer claimableNamespacesLock.Unlock()
-
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-
-			default:
-				break
-			}
-
-			if hook := hooks.OnBeforeCreateNamespace; hook != nil {
-				hook(id)
-			}
-
-			namespace := network.NewNamespace(
-				id,
-
-				hostInterface,
-				namespaceInterface,
-
-				namespaceInterfaceGateway,
-				namespaceInterfaceNetmask,
-
-				hostVeth.GetFirstIP().String(),
-				hostVeth.GetSecondIP().String(),
-
-				namespaceInterfaceIP,
-				namespaceVeth.String(),
-
-				blockedSubnetCIDR,
-
-				namespaceInterfaceMAC,
-
-				allowIncomingTraffic,
-			)
-			if err := namespace.Open(); err != nil {
-				if e := namespace.Close(); e != nil {
-					return errors.Join(ErrCouldNotOpenNamespace, err, e)
-				}
-
-				return err
-			}
-
-			claimableNamespaces[id] = claimableNamespace{
-				namespace: namespace,
-			}
-
-			return nil
-		}(); err != nil {
-			panic(errors.Join(ErrCouldNotOpenNamespace, err))
-		}
-	}
-
-	namespaces.ClaimNamespace = func() (string, error) {
-		claimableNamespacesLock.Lock()
-		defer claimableNamespacesLock.Unlock()
-
-		for _, namespace := range claimableNamespaces {
-			if !namespace.claimed {
-				namespace.claimed = true
-
-				return namespace.namespace.GetID(), nil
-			}
-		}
-
-		return "", ErrAllNamespacesClaimed
-	}
-
-	namespaces.ReleaseNamespace = func(namespace string) error {
-		claimableNamespacesLock.Lock()
-		defer claimableNamespacesLock.Unlock()
-
-		ns, ok := claimableNamespaces[namespace]
-		if !ok {
-			// Releasing non-claimed namespaces is a no-op
-			return nil
-		}
-
-		ns.claimed = false
-
-		return nil
 	}
 
 	cancelReadyCtx()
 
-	return
+	return namespaces, nil
+}
+
+type NamespacesManager struct {
+	config namespacesManagerConfig
+
+	closeInProgressCtx       context.Context
+	cancelCloseInProgressCtx func()
+
+	closed     bool
+	closedLock sync.Mutex
+
+	hostVeths     []*network.IPPair
+	hostVethsLock sync.Mutex
+
+	namespaceVeths     []*network.IP
+	namespaceVethsLock sync.Mutex
+
+	claimableNamespaces     map[string]claimableNamespace
+	claimableNamespacesLock sync.Mutex
+}
+
+type claimableNamespace struct {
+	namespace *network.Namespace
+	claimed   bool
+}
+
+type namespacesManagerConfig struct {
+	ctx                       context.Context
+	closeCtx                  context.Context
+	hostInterface             string
+	namespaceInterface        string
+	namespaceInterfaceGateway string
+	namespaceInterfaceNetmask uint32
+	namespaceInterfaceIP      string
+	namespaceInterfaceMAC     string
+	blockedSubnetCIDR         string
+	allowIncomingTraffic      bool
+	hostVethIPs               *network.IPTable
+	namespaceVethIPs          *network.IPTable
+	hooks                     CreateNamespacesHooks
+}
+
+func newNamespacesManager(config namespacesManagerConfig) *NamespacesManager {
+	nsm := &NamespacesManager{
+		config:              config,
+		claimableNamespaces: make(map[string]claimableNamespace),
+	}
+	nsm.closeInProgressCtx, nsm.cancelCloseInProgressCtx = context.WithCancel(config.closeCtx) // We use `closeContext` here since this simply intercepts `ctx`
+
+	return nsm
+}
+
+func (nss *NamespacesManager) ClaimNamespace() (string, error) {
+	nss.claimableNamespacesLock.Lock()
+	defer nss.claimableNamespacesLock.Unlock()
+
+	for _, namespace := range nss.claimableNamespaces {
+		if !namespace.claimed {
+			namespace.claimed = true
+			return namespace.namespace.GetID(), nil
+		}
+	}
+
+	return "", ErrAllNamespacesClaimed
+}
+
+func (nss *NamespacesManager) ReleaseNamespace(namespace string) error {
+	nss.claimableNamespacesLock.Lock()
+	defer nss.claimableNamespacesLock.Unlock()
+
+	ns, ok := nss.claimableNamespaces[namespace]
+	if !ok {
+		// Releasing non-claimed namespaces is a no-op
+		return nil
+	}
+	ns.claimed = false
+
+	return nil
+}
+
+// Future-proofing; if we decide that NATing should use a background copy loop like `socat`, we can wait for that loop to finish here and return any errors
+func (nss *NamespacesManager) Wait() error {
+	<-nss.closeInProgressCtx.Done()
+	return nil
+}
+
+func (nss *NamespacesManager) Close() (errs error) {
+	defer nss.cancelCloseInProgressCtx()
+
+	nss.hostVethsLock.Lock()
+	defer nss.hostVethsLock.Unlock()
+
+	for _, hostVeth := range nss.hostVeths {
+		if err := nss.config.hostVethIPs.ReleasePair(nss.config.closeCtx, hostVeth); err != nil {
+			errs = errors.Join(errs, ErrCouldNotReleaseHostVethIP, err)
+		}
+	}
+	nss.hostVeths = []*network.IPPair{}
+
+	nss.namespaceVethsLock.Lock()
+	defer nss.namespaceVethsLock.Unlock()
+
+	for _, namespaceVeth := range nss.namespaceVeths {
+		if err := nss.config.namespaceVethIPs.ReleaseIP(nss.config.closeCtx, namespaceVeth); err != nil {
+			errs = errors.Join(errs, ErrCouldNotReleaseNamespaceVethIP, err)
+		}
+	}
+	nss.namespaceVeths = []*network.IP{}
+
+	nss.claimableNamespacesLock.Lock()
+	defer nss.claimableNamespacesLock.Unlock()
+
+	for _, claimableNamespace := range nss.claimableNamespaces {
+		if hook := nss.config.hooks.OnBeforeRemoveNamespace; hook != nil {
+			hook(claimableNamespace.namespace.GetID())
+		}
+
+		if err := claimableNamespace.namespace.Close(); err != nil {
+			errs = errors.Join(errs, ErrCouldNotCloseNamespace, err)
+		}
+	}
+	nss.claimableNamespaces = map[string]claimableNamespace{}
+
+	nss.closedLock.Lock()
+	defer nss.closedLock.Unlock()
+
+	if !nss.closed {
+		nss.closed = true
+
+		if err := network.RemoveNAT(nss.config.hostInterface); err != nil {
+			errs = errors.Join(errs, ErrCouldNotRemoveNAT, err)
+		}
+	}
+
+	// No need to call `.Wait()` here since `.Wait()` is just waiting for us to cancel the in-progress context
+
+	return errs
+}
+
+func (nss *NamespacesManager) addClaimableNamespace(id string) error {
+	hostVeth, err := nss.openHostVeth()
+	if err != nil {
+		return errors.Join(ErrCouldNotOpenHostVethIPs, err)
+	}
+	nss.hostVeths = append(nss.hostVeths, hostVeth)
+
+	namespaceVeth, err := nss.openNamespaceVeth()
+	if err != nil {
+		return errors.Join(ErrCouldNotOpenNamespaceVethIPs, err)
+	}
+	nss.namespaceVeths = append(nss.namespaceVeths, namespaceVeth)
+
+	namespace, err := nss.openNamespace(id, hostVeth, namespaceVeth)
+	if err != nil {
+		return errors.Join(ErrCouldNotOpenNamespace, err)
+	}
+	nss.claimableNamespaces[id] = claimableNamespace{
+		namespace: namespace,
+	}
+
+	return nil
+}
+
+func (nss *NamespacesManager) openHostVeth() (*network.IPPair, error) {
+	nss.hostVethsLock.Lock()
+	defer nss.hostVethsLock.Unlock()
+
+	hostVeth, err := nss.config.hostVethIPs.GetPair(nss.config.ctx)
+	if err != nil {
+		if e := nss.config.hostVethIPs.ReleasePair(nss.config.closeCtx, hostVeth); e != nil {
+			return nil, errors.Join(ErrCouldNotReleaseHostVethIP, err, e)
+		}
+
+		return nil, err
+	}
+
+	return hostVeth, nil
+}
+
+func (nss *NamespacesManager) openNamespaceVeth() (*network.IP, error) {
+	nss.namespaceVethsLock.Lock()
+	defer nss.namespaceVethsLock.Unlock()
+
+	namespaceVeth, err := nss.config.namespaceVethIPs.GetIP(nss.config.ctx)
+	if err != nil {
+		if e := nss.config.namespaceVethIPs.ReleaseIP(nss.config.closeCtx, namespaceVeth); e != nil {
+			return nil, errors.Join(ErrCouldNotReleaseNamespaceVethIP, err, e)
+		}
+
+		return nil, err
+	}
+
+	return namespaceVeth, nil
+}
+
+func (nss *NamespacesManager) openNamespace(id string, hostVeth *network.IPPair, namespaceVeth *network.IP) (*network.Namespace, error) {
+	nss.claimableNamespacesLock.Lock()
+	defer nss.claimableNamespacesLock.Unlock()
+
+	if hook := nss.config.hooks.OnBeforeCreateNamespace; hook != nil {
+		hook(id)
+	}
+
+	namespace := network.NewNamespace(
+		id,
+
+		nss.config.hostInterface,
+		nss.config.namespaceInterface,
+
+		nss.config.namespaceInterfaceGateway,
+		nss.config.namespaceInterfaceNetmask,
+
+		hostVeth.GetFirstIP().String(),
+		hostVeth.GetSecondIP().String(),
+
+		nss.config.namespaceInterfaceIP,
+		namespaceVeth.String(),
+
+		nss.config.blockedSubnetCIDR,
+
+		nss.config.namespaceInterfaceMAC,
+
+		nss.config.allowIncomingTraffic,
+	)
+	if err := namespace.Open(); err != nil {
+		if e := namespace.Close(); e != nil {
+			return nil, errors.Join(err, e)
+		}
+
+		return nil, err
+	}
+
+	return namespace, nil
 }
